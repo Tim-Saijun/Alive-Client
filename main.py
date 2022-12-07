9bfba2a2f1196c5af9b53569bb09c6ba0221d4c5
杨再润 <yangzair@outlook.com>
2022-12-07 15:01:52 +0800

    [实现]测距评分

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-
-###########################################################################################
-###                        CODE:       WRITTEN BY: ANJAL.P AUGUST 11 2020               ###
-###                        PROJECT:    PELLIS Z1                                        ###
-###                        PURPOSE:    WINDOWS/LINUX/MAC OS FLAT MODERN UI              ###
-###                                    BASED ON QT DESIGNER, PySide2                    ###
-###                        USE CASE:   TEMPLATE FOR SOFTWARES                           ###
-###                        LICENCE:    MIT OPENSOURCE LICENCE                           ###
-###                                                                                     ###
-###                            CODE IS FREE TO USE AND MODIFY                           ###
-###########################################################################################
-
 ###########################################################################################
 #                                     DOCUMENTATION                                       #
 #                                                                                         #
 #  Each line of the code described below is commented well, such as: the purpose of the   #
 #  code, its function, returns e.t.c as in certain case: the alternatives to that solul-  #
 #  ution, other sources like included PDF document has also the working of the code.      #
-#  CSS stylesheet of the buttons are given seperatly in the CSS.txt in the parent folder  #
+#  CSS stylesheet of the buttons are given seperatly in the CSS.txt in the parent folder
+#  下面描述的每一行代码都有很好的注释，例如：代码的目的，它的功能，
+#  在某些情况下返回等：该解决方案的替代方案，其他来源如包含的 PDF 文档也具有代码。
+#  按钮的 CSS 样式表在父文件夹的 CSS.txt 中单独给出
+#  #
 ###########################################################################################
 
 ###########################################################################################
@@ -27,7 +19,12 @@
 #  CTION WHAY HAPPENS AFTER THIS I.E. WHEN THE USER CHANGES THE INPUT STATE, ONLY IS DELT #
 #  HERE, SO IF YOU WANT TO MODIFY THE FILE, PLEASE OPEN THE CORRESPONDING .ui FILE IN QT  #
 #  DESIGNER AND MADE THE MODIFICATION AND THENY COME BACK HERE TO ADD FUNCTIONALITY TO THE#
-#  CHANGES.                                                                               #
+#  CHANGES.
+# 注意 由于大部分工作是在 QT DESIGNER 中完成的，因此您可能看不到此处的样式表，
+# 请参阅 Documentation.pdf 此外，许多设置已在 QT DESIGNER 本身中预定义，
+# 因此在此函数中会发生什么在这之后当用户更改输入状态时，此处仅删除，因此如果您想修改该文件，
+# 请在 QT 设计器中打开相应的 .ui 文件并进行修改，然后返回此处为更改添加功能。
+#  #
 ###########################################################################################
 import os.path
 import sys
@@ -378,6 +375,7 @@ class MainWindow(QMainWindow):
         #TODO:空图片的判定驳回
         # self.ui.plainTextEdit_9.setPlainText('356')#设置值
         # abc = self.ui.plainTextEdit_9.toPlainText()#读值
+        #TODO:AI测距前先要求输入评分
         img_name = os.path.basename(self.img)
         print("推送图像：",img_name)
         para = {'img_name':img_name}
@@ -417,6 +415,23 @@ class MainWindow(QMainWindow):
         self.ui.plainTextEdit_19.setPlainText(str(int(self.whs[4][0])) + ' ' + str(int(self.whs[4][1])))
 
     def evaluate(self):
+        #TODO:评分前先检查是否进行AI测距并得到结果
+        origin_list = []
+        origin_list.append(list(map(int,self.ui.plainTextEdit_6.toPlainText().split())))
+        origin_list.append(list(map(int, self.ui.plainTextEdit_9.toPlainText().split())))
+        origin_list.append(list(map(int, self.ui.plainTextEdit_7.toPlainText().split())))
+        origin_list.append(list(map(int, self.ui.plainTextEdit_8.toPlainText().split())))
+        origin_list.append(list(map(int, self.ui.plainTextEdit_10.toPlainText().split())))
+        mark = 0
+        for r1 , r2 in zip(origin_list,self.whs):
+            area1 = r1[0]*r1[1]
+            are2 = r2[0]*r2[1]
+            tp1 = min(are2,area1)
+            tp2 = max(area1,are2)
+            mark+=(tp1/tp2)/5
+        mark = mark*100
+        self.dialogexec("评分结果","您的测距结果与AI测距结果的匹配得分为：%f分"%mark,icon=r"icons/1x/errorAsset 55.png",btn1="好",btn2="好")
+        print(mark)
         pass
 if __name__ == "__main__":
     app = QApplication(sys.argv)
